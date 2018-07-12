f272450fb99586f5615b5ff2d5f92194acad10bb
Jenny <fantazmit@gmail.com>
2018-07-12 23:18:34 +0200

    Full short project

---
+++ FileServer.Infraestructure.Repository.Behaviour.Test/StudentRepositoryInsert.feature
@@ -0,0 +1,8 @@
+﻿Feature: StudentRepositoryInsert
+	Insert Student in Student Repository
+
+@adding
+Scenario: Add two numbers
+	Given I have an Student without Id
+	When I run the method Insert of STudent Repository
+	Then the Student Repository should return a Student with Id
---
+++ FileServer.Infraestructure.Repository.Behaviour.Test/StudentRepositoryInsertSteps.cs
@@ -0,0 +1,36 @@
+﻿using FileServer.Infraestructure.Repository.Repositories;
+using System;
+using TechTalk.SpecFlow;
+using FluentAssertions;
+
+namespace FileServer.Infraestructure.Repository.Behaviour.Test
+{
+    [Binding]
+    public class StudentRepositoryInsertSteps
+    {
+        protected StudentRepository repository = new StudentRepository();
+
+        protected Student student;
+
+        [Given(@"I have an Student without Id")]
+        public void GivenIHaveAnStudentWithoutId()
+        {
+            student = new Student(new DateTime(1990, 6, 22));
+        }
+        
+        [When(@"I run the method Insert of STudent Repository")]
+        public void WhenIRunTheMethodInsertOfSTudentRepository()
+        {
+            repository.Insert(student);
+        }
+        
+        [Then(@"the Student Repository should return a Student with Id")]
+        public void ThenTheStudentRepositoryShouldReturnAStudentWithId()
+        {
+            student.Id.Should().Be(1);
+
+            student.Age.Should().Be(28);
+
+        }
+    }
+}
